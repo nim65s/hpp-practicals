c51a95db049f64021798c3c92c4a2ab5c8320cd6
tpplanning <stonneau@hotmail.fr>
2016-12-09 09:02:11 +0100

    saving previous group work

--- update/save.sh
+++ update/save.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd $DEVEL_DIR/src/hpp-practicals/script
-mv rrt.py ../rrt_g1.py
-mv motion_planner.py ../motion_planner_g1.py
-git checkout rrt.py
-git checkout motion_planner.py
+cd $DEVEL_DIR/src/hpp-practicals/script/ik
+mv ik.py ../../ik_g1.py
+git checkout ik.py
 
 
