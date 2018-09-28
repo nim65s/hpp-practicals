cd499fdb0c5b5c266299a2627a8b4ef688f4bbb2
tpplanning <tt@t.com>
2018-09-28 10:47:18 +0200

    updated save.sh

--- update/save.sh
+++ update/save.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd $DEVEL_DIR/src/hpp-practicals/script/
+mv motion_planner.py ../../motion_planner_g1.py
+git checkout motion_planner.py
+mv rrt.py ../../rrt_g1.py
+git checkout rrt.py
+
+
 cd $DEVEL_DIR/src/hpp-practicals/script/ik
 mv ik.py ../../ik_g1.py
 git checkout ik.py
