ec5f5cfc0b5f0e3962021ec560d3ee3d63a3112d
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-06 21:23:00 +0100

    update packaging

--- flake.nix
+++ flake.nix
@@ -13,20 +13,14 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          packages = {
-            default = self'.packages.hpp-practicals;
-            hpp-practicals = pkgs.python3Packages.hpp-practicals.overrideAttrs {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          {
+            gazebros2nix.overrides.hpp-practicals = _final: {
               src = lib.fileset.toSource {
                 root = ./.;
                 fileset = lib.fileset.unions [
@@ -44,7 +38,8 @@
                 ];
               };
             };
-          };
-        };
-    };
+          }
+        ];
+      }
+    );
 }
