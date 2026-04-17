a25eca060ce314729bf761ffbb082f5a2483741b
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-17 11:24:59 +0200

    fix v8.0.0 merge

--- flake.nix
+++ flake.nix
@@ -1,45 +1,31 @@
 {
   description = "Practicals for Humanoid Path Planner software";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            gazebros2nix.overrides.hpp-practicals = _final: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./docker
-                  ./instructions
-                  ./meshes
-                  ./package.xml
-                  ./script
-                  ./slides
-                  ./src
-                  ./srdf
-                  ./update
-                  ./urdf
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-practicals = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./docker
+              ./instructions
+              ./meshes
+              ./package.xml
+              ./script
+              ./slides
+              ./src
+              ./srdf
+              ./update
+              ./urdf
+            ];
+          };
+        };
       }
     );
 }
